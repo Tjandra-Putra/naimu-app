7c640f84bc91e38139ab078cf29937d080de3312
Tjandra Putra <tjandrap.work@gmail.com>
2023-04-27 22:38:21 +0800

    update: UI/UX improvements

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -278,6 +278,34 @@ const Products = () => {
                 </div>
               </div>
             </div>
+
+            <nav aria-label="Page navigation example">
+              <ul class="pagination justify-content-center mt-3">
+                <li class="page-item disabled">
+                  <a class="page-link text-dark">Previous</a>
+                </li>
+                <li class="page-item">
+                  <a class="page-link text-dark" href="#">
+                    1
+                  </a>
+                </li>
+                <li class="page-item">
+                  <a class="page-link text-dark" href="#">
+                    2
+                  </a>
+                </li>
+                <li class="page-item">
+                  <a class="page-link text-dark" href="#">
+                    3
+                  </a>
+                </li>
+                <li class="page-item">
+                  <a class="page-link text-dark" href="#">
+                    Next
+                  </a>
+                </li>
+              </ul>
+            </nav>
           </div>
         </div>
       </div>
