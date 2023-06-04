f4e6afb4ae5fc8870af5c25613b9608504c6aba1
Tjandra Putra <tjandrap.work@gmail.com>
2023-06-04 14:44:14 +0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,7 @@ const cors = require("cors");
 
 // allow requests from localhost:3000
 const corsOptions = {
-  // origin: "http://localhost:3000",
-  origin: "https://naimu-app.vercel.app/",
+  origin: "http://localhost:3000",
   credentials: true,
 };
 
