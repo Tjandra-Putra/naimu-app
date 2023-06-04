028199ba2c5e8b4cbf9ec9f4241b04100612775a
Tjandra Putra <tjandrap.work@gmail.com>
2023-06-04 17:29:27 +0800

    attempt fix jwt token

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -7,8 +7,7 @@ const sendToken = (user, statusCode, res) => {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000), // 90 days
     httpOnly: true,
     secure: true, // only sent over a secure (HTTPS) connection
-    // domain: process.env.NODE_ENV !== "PRODUCTION" ? "localhost" : "https://naimu-app.vercel.app",
-    // sameSite: "None", // explicitly set the SameSite attribute to "None"
+    domain: process.env.NODE_ENV !== "PRODUCTION" ? "localhost:3000" : "naimu-app.vercel.app", // Set the domain to match your frontend domain
   };
 
   res.status(statusCode).cookie("token", token, options).json({
