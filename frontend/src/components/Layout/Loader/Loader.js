279a3dd5739a5b1bdf3d35606d561e81c15bb113
Tjandra Putra <tjandrap.work@gmail.com>
2023-06-07 18:19:43 +0800

    loader ui

--- frontend/src/components/Layout/Loader/Loader.js
+++ frontend/src/components/Layout/Loader/Loader.js
@@ -1,12 +1,14 @@
 import "./Loader.css";
 import BarLoader from "react-spinners/BarLoader";
+import HashLoader from "react-spinners/HashLoader";
+import PropagateLoader from "react-spinners/PropagateLoader";
 
 const Loader = () => {
   return (
     <div className="loader-wrapper">
       {/* set duration to loader */}
-
-      <BarLoader loading={true} size={150} aria-label="Loading Spinner" data-testid="loader" color="#ffffff" />
+      {/* barloader size is 150 */}
+      <PropagateLoader loading={true} size={30} aria-label="Loading Spinner" data-testid="loader" color="#ffffff" />
     </div>
   );
 };
